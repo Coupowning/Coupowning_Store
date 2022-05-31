b3f9ebc03df96630635e98f2dc98a6b0a1abeff7
ohksj77 <ohksj77@gmail.com>
2022-05-31 23:04:12 +0900

    add : Naver Map API Marker 찍기
    완료

--- app/src/main/java/kr/ac/coukingmama/storeapp/before/AddressActivity.kt
+++ app/src/main/java/kr/ac/coukingmama/storeapp/before/AddressActivity.kt
@@ -1,89 +1,87 @@
 package kr.ac.coukingmama.storeapp.before
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
-import android.webkit.WebView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import kr.ac.coukingmama.storeapp.R
 import kr.ac.coukingmama.storeapp.databinding.ActivityAddressBinding
 
 
-class AddressActivity : AppCompatActivity() {
-
+class AddressActivity : AppCompatActivity(), OnMapReadyCallback {
+    var slatLng : LatLng? = null
+    val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+    var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
     lateinit var binding : ActivityAddressBinding
-    lateinit var webView : WebView
+    var strings : ArrayList<String> = arrayListOf()
+    lateinit var mapView : MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddressBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        webView = binding.webView
-//        showAddressWebView()
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        mapView = binding.navermap
+        mapView.getMapAsync(this)
+        strings = intent.getStringArrayListExtra("inform")!!
     }
-//
-//    private fun showAddressWebView() {
-//        webView.settings.apply {
-//            javaScriptEnabled = true
-//            javaScriptCanOpenWindowsAutomatically = true
-//            setSupportMultipleWindows(true)
-//        }
-//        webView.apply {
-//            addJavascriptInterface(WebViewData(), "coupowning")
-//            webViewClient = client
-//            webChromeClient = chromeClient
-//            loadUrl("https://coupowning.web.app/")
-//        }
-//    }
-//
-//    private val client: WebViewClient = object : WebViewClient() {
-//        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-//            return false
-//        }
-//        override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
-//            handler?.proceed()
-//        }
-//    }
-//
-//    private inner class WebViewData {
-//        @JavascriptInterface
-//        fun getAddress(roadAddress: String) {
-//            CoroutineScope(Dispatchers.Default).launch {
-//                withContext(CoroutineScope(Dispatchers.Main).coroutineContext) {
-//                    val address = roadAddress
-//                    Toast.makeText(applicationContext, address, Toast.LENGTH_SHORT).show()
-//                    startActivity(Intent(applicationContext, RegisterActivity::class.java).putExtra("address", address))
-//                    finish()
-//                }
-//            }
-//        }
-//    }
-//
-//    private val chromeClient = object : WebChromeClient() {
-//
-//        override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
-//
-//            val newWebView = WebView(this@AddressActivity)
-//            newWebView.settings.javaScriptEnabled = true
-//            val dialog = Dialog(this@AddressActivity)
-//            dialog.setContentView(newWebView)
-//
-//            val params = dialog.window!!.attributes
-//
-//            params.width = ViewGroup.LayoutParams.MATCH_PARENT
-//            params.height = ViewGroup.LayoutParams.MATCH_PARENT
-//            dialog.window!!.attributes = params
-//            dialog.show()
-//
-//            newWebView.webChromeClient = object : WebChromeClient() {
-//                override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
-//                    super.onJsAlert(view, url, message, result)
-//                    return true
-//                }
-//                override fun onCloseWindow(window: WebView?) {
-//                    dialog.dismiss()
-//                }
-//            }
-//            (resultMsg!!.obj as WebView.WebViewTransport).webView = newWebView
-//            resultMsg.sendToTarget()
-//            return true
-//        }
-//    }
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.33992342806354, 126.73348888791088))
+        naverMap.moveCamera(cameraUpdate)
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+        uiSettings.isZoomGesturesEnabled = true
+        uiSettings.isScrollGesturesEnabled = true
+        uiSettings.isTiltGesturesEnabled = false
+        uiSettings.isRotateGesturesEnabled = false
+
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, false)
+        val marker = Marker()
+        marker.position = LatLng(37.33992342806354, 126.73348888791088)
+        marker.map = naverMap
+        marker.icon =  OverlayImage.fromResource(R.drawable.ic_marker)
+        marker.width = 100
+        marker.height = 100
+        naverMap.setOnMapClickListener { pointF, latLng ->
+            marker.position = latLng
+            slatLng = latLng
+        }
+        binding.saveButton.setOnClickListener{
+            if(slatLng != null){
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.putExtra("x", slatLng!!.latitude)
+                intent.putExtra("y", slatLng!!.longitude)
+                intent.putExtra("inform", strings)
+                Toast.makeText(this, "위치가 등록되었습니다", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.size > 0
+                && grantResults[0] == PERMISSION_GRANTED
+            ) {
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 }
--- app/src/main/java/kr/ac/coukingmama/storeapp/before/RegisterActivity.kt
+++ app/src/main/java/kr/ac/coukingmama/storeapp/before/RegisterActivity.kt
@@ -32,6 +32,8 @@ class RegisterActivity : AppCompatActivity() { // 가게 등록 페이지
     private var num : Int = 1
     private val GET_GALLERY_IMAGE : Int = 200
     lateinit var strings : ArrayList<String>
+    private var longitude : Double? = null
+    private var latitude : Double? = null
 
     @SuppressLint("UnsafeOptInUsageError")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,9 +68,8 @@ class RegisterActivity : AppCompatActivity() { // 가게 등록 페이지
             }
         }
 
-        if(intent.getStringExtra("address") != null)
-            binding.etaddress.setText(intent.getStringExtra("address"))
-
+        latitude = intent.getDoubleExtra("x", -1.0)
+        longitude = intent.getDoubleExtra("y", -1.0)
 
         binding.listItem.adapter = listAdapter
         binding.listItem.layoutManager =
@@ -84,53 +85,58 @@ class RegisterActivity : AppCompatActivity() { // 가게 등록 페이지
             startActivityForResult(intent, GET_GALLERY_IMAGE)
         }
         binding.modifystore.setOnClickListener {
-            var storeInfo: Store? = null
-            if (binding.intro.text.toString().trim().isNotEmpty() && binding.stampsum.text.toString().trim().isNotEmpty() && binding.num.text.toString().trim().isNotEmpty() && binding.award.text.toString().trim().isNotEmpty() &&
-                binding.phonenum.text.toString().trim().isNotEmpty() && binding.etstorename.text.toString().trim().isNotEmpty()
-            ) {
-                val images = hashMapOf<Int, String>() // 이미지 정보는 추후에 DB에서 뺄 것
-                images[1] = "www.a.b"
-                storeInfo = Store(
-                    binding.etstorename.text.toString(),
-                    StoreLocation("경기도 시흥시 산기대학로...", "2,1", "3.2"),
-                    binding.phonenum.text.toString(),
-                    binding.intro.text.toString() + "/" +  binding.stampsum.text.toString().toInt() + "/" + binding.num.text.toString() + "/" + binding.award.text.toString(),
-                    "cafe302", // id 변경해줄 것
-                    images)
+            if(latitude == -1.0 && longitude == -1.0){
+                Toast.makeText(applicationContext, "지도에 가게를 표시해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var storeInfo: Store? = null
+                if (binding.intro.text.toString().trim().isNotEmpty() && binding.stampsum.text.toString().trim().isNotEmpty() && binding.num.text.toString().trim().isNotEmpty() && binding.award.text.toString().trim().isNotEmpty() &&
+                    binding.phonenum.text.toString().trim().isNotEmpty() && binding.etstorename.text.toString().trim().isNotEmpty()
+                ) {
+                    val images = hashMapOf<Int, String>() // 이미지 정보는 추후에 DB에서 뺄 것
+                    images[1] = "www.a.b"
+                    storeInfo = Store(
+                        binding.etstorename.text.toString(),
+                        StoreLocation("경기도 시흥시 산기대학로...", "$latitude", "$longitude"),
+                        binding.phonenum.text.toString(),
+                        binding.intro.text.toString() + "/" +  binding.stampsum.text.toString().toInt() + "/" + binding.num.text.toString() + "/" + binding.award.text.toString(),
+                        "cafe302", // id 변경해줄 것
+                        images)
 
-                val api = StoreService.create()
-                val callPost = api.postStore(storeInfo).enqueue(object : Callback<Store> {
-                    override fun onResponse(call: Call<Store>, response: Response<Store>) {
-                        if(response.isSuccessful)
-                            Log.d("response", " HTTP Status Code > ${response.code()} \n ${response.body()}")
-                        else
-                            Log.d("response", " HTTP Status Code > ${response.code()}")
-                    }
-                    override fun onFailure(call: Call<Store>, t: Throwable) {
-                        Log.d("error", "ERROR message > " + t.message.toString())
-                    }
-                })
-                val storage = FirebaseStorage.getInstance()
-                val storageRef = storage.reference
-                val fileName = "store${num++}.jpg"
-                val imageStorageRef = storageRef.child("image").child(fileName) // "image" 변경하기
-                uriList.forEach {
-                    val uploadTask = imageStorageRef.putFile(it)
-                    uploadTask.addOnFailureListener {
-                        it.printStackTrace()
-                    }.addOnSuccessListener {
-                        Toast.makeText(this, "가게가 등록/수정 되었습니다!", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, MainActivity::class.java).putExtra("registered", true)
-                        startActivity(intent)
-                        finish()
-                    }.addOnCanceledListener {
-                        Toast.makeText(this, "<오류>", Toast.LENGTH_SHORT).show()
+                    val api = StoreService.create()
+                    val callPost = api.postStore(storeInfo).enqueue(object : Callback<Store> {
+                        override fun onResponse(call: Call<Store>, response: Response<Store>) {
+                            if(response.isSuccessful)
+                                Log.d("response", " HTTP Status Code > ${response.code()} \n ${response.body()}")
+                            else
+                                Log.d("response", " HTTP Status Code > ${response.code()}")
+                        }
+                        override fun onFailure(call: Call<Store>, t: Throwable) {
+                            Log.d("error", "ERROR message > " + t.message.toString())
+                        }
+                    })
+                    val storage = FirebaseStorage.getInstance()
+                    val storageRef = storage.reference
+                    val fileName = "store${num++}.jpg"
+                    val imageStorageRef = storageRef.child("image").child(fileName) // "image" 변경하기
+                    uriList.forEach {
+                        val uploadTask = imageStorageRef.putFile(it)
+                        uploadTask.addOnFailureListener {
+                            it.printStackTrace()
+                        }.addOnSuccessListener {
+                            Toast.makeText(this, "가게가 등록/수정 되었습니다!", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java).putExtra("registered", true)
+                            startActivity(intent)
+                            finish()
+                        }.addOnCanceledListener {
+                            Toast.makeText(this, "<오류>", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
         }
         binding.search.setOnClickListener{
-            val intent = Intent(this, AddressActivity::class.java) // 주소 검색 페이지
+            val intent = Intent(this, AddressActivity::class.java).putExtra("inform", strings) // 주소 검색 페이지
             startActivity(intent)
         }
     }
